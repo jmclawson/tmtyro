03f454c61e907955279e3ceb6edb9cf99f36aab2
James Clawson <clawson@gmail.com>
2025-01-27 10:58:51 -0600

    updated dependency, finally finish replacing `count()` with `add_frequency()`

--- R/borrowed.R
+++ R/borrowed.R
@@ -12,19 +12,19 @@ drop_na <- function(data, ...){
   tidyr::drop_na(data, ...)
 }
 
-#' Count values in one or more columns
+#' #' Count values in one or more columns
+#' #'
+#' #' @param x A data frame
+#' #' @param ... The columns to count. If more than one column is chosen, combinations of values are counted.
+#' #'
+#' #' @returns A data frame with one column for every grouping identified by `...` and an additional column for the number of values counted
+#' #' @export
+#' #'
+#' #' @examples
+#' #' mtcars |>
+#' #'   count(cyl)
+#' count <- function(x, ...){
+#'   dplyr::count(x, ...) |>
+#'     add_class("count")
+#' }
 #'
-#' @param x A data frame
-#' @param ... The columns to count. If more than one column is chosen, combinations of values are counted.
-#'
-#' @returns A data frame with one column for every grouping identified by `...` and an additional column for the number of values counted
-#' @export
-#'
-#' @examples
-#' mtcars |>
-#'   count(cyl)
-count <- function(x, ...){
-  dplyr::count(x, ...) |>
-    add_class("count")
-}
-
